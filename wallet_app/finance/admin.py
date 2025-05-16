4d2efa8491646994e0a24f87e0bee2a23da19f48
Kathoni <thrusherkeys@gmail.com>
2025-05-16 16:00:11 +0300

    project reform

--- wallet_app/finance/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import Transaction, Budget, Goal
-
-admin.site.register(Transaction)
-admin.site.register(Budget)
-admin.site.register(Goal)
